7a061262d1f5b7fdb395867651abf72cc036ee82
jihoon <jibbong001@gmail.com>
2026-01-01 11:46:58 +0900

    wifi_interface 노드의 쓰레기 파일 제거

--- pkgs/wifi_interface/install/wifi_interface/local/lib/python3.10/dist-packages/wifi_interface/msg/__init__.py
+++
@@ -1,2 +0,0 @@
-from wifi_interface.msg._wifi_fused import WifiFused  # noqa: F401
-from wifi_interface.msg._wifi_rssi import WifiRssi  # noqa: F401
--- pkgs/wifi_interface/install/wifi_interface/share/wifi_interface/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(wifi_interface_IDL_FILES "msg/WifiRssi.idl;msg/WifiFused.idl")
-set(wifi_interface_INTERFACE_FILES "msg/WifiRssi.msg;msg/WifiFused.msg")
